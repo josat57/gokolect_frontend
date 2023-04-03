05eda055b9e7a224b388d971993a9376c5bf2250
Samuel Joseph Tamunobarasinipiri <joseph.samuel@cinfores.com>
2023-04-03 14:55:25 +0100

    updated

--- server/index.php
+++ server/index.php
@@ -32,6 +32,7 @@ header('Content-Type: application/json');
 // }
 
 $response = $_POST;
+die(var_dump($response));
 if (isset($_POST['data']) && isset($_POST['id']) && isset($_POST['action'])) {    
     if ($_POST['action'] === 'items') {
         $response = uploadItems($_POST['id'], $_POST['data'], $_POST['imageFileType'], $_POST['dir']);
